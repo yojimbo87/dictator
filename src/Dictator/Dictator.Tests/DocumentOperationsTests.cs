21e64cfb88ed344da94dd2455fb33c2b5a7b83c5
Tomas Bosak <bosak.tomas@gmail.com>
2014-12-04 13:33:08 +0100

    Implement merge operation and cover it with test.

---
+++ src/Dictator/Dictator.Tests/DocumentOperationsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Dictator.Tests
+{
+    [TestFixture()]
+    public class DocumentOperationsTests
+    {
+        [Test()]
+        public void Should_merge_document_fields()
+        {
+            var doc1 = Dictator.New()
+                .String("foo", "string value")
+                .Int("bar", 12345);
+            
+            var doc2 = Dictator.New()
+                .String("foo", "new string value")
+                .Int("baz", 54321);
+            
+            doc1.Merge(doc2);
+            
+            Assert.AreEqual(doc1.Count, 3);
+            Assert.AreEqual(doc1.String("foo"), "new string value");
+            Assert.AreEqual(doc1.Int("bar"), 12345);
+            Assert.AreEqual(doc1.Int("baz"), 54321);
+            
+            Assert.AreEqual(doc2.Count, 2);
+        }
+    }
+}
